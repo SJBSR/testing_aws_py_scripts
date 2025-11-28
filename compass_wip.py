9615a2144bb12f65828a162ec741618773120021
SJBSR <sjb.sr@outlook.com>
2025-11-28 18:50:26 -0500

    action

--- compass_wip.py
+++ compass_wip.py
@@ -1,7 +1,9 @@
 import load_secrets_wip
 
 def main():
-    secret_name = "my_secret_name"
+    secret_name = "AWS_SECRET_KEY_ID"
     load_secrets_wip.get_secret(secret_name)
     print("Secret retrieved successfully.")
+    secret_key = "AWS_SECRET_KEY"
+    load_secrets_wip.get_secret(secret_key)
 
